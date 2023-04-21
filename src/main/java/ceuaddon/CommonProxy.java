676e6146a6ed1588d754e5da8f7fac6472a621dc
BL00DKNIGHT <58563153+BL00DKNIGHT@users.noreply.github.com>
2023-04-21 18:34:35 -0300

    amongi
    i didn't made some textures yet

--- src/main/java/ceuaddon/CommonProxy.java
+++ src/main/java/ceuaddon/CommonProxy.java
@@ -7,12 +7,20 @@ import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+import gregtech.api.unification.ore.OrePrefix; //Needed for oreprefix/unification
+import gregtech.api.GregTechAPI; //Used to interact with CEu's creative tabs and events like HighTier
+
+import gregtech.api.items.metaitem.ElectricStats; //Needed for PotatOS (battery)
+import gregtech.api.GTValues; //To set voltages
+import gregtech.api.unification.material.MarkerMaterials.Tier; //To set tiers
 @Mod.EventBusSubscriber(modid = CEuAddon.ID)
 public class CommonProxy {
 
     public static MetaItem<?> exampleitems;
 
     public static MetaItem<?>.MetaValueItem exampleSusso;
+    public static MetaItem<?>.MetaValueItem POTATOS;
+    public static MetaItem<?>.MetaValueItem POTATOS_ULV;
 
     public void preLoad() {
         exampleitems = new StandardMetaItem();
@@ -22,5 +30,7 @@ public class CommonProxy {
     @SubscribeEvent
     public static void registerItems(RegistryEvent.Register<Item> event) {
         exampleSusso = exampleitems.addItem(0, "example_susso_meta_item");
+        POTATOS = exampleitems.addItem(1, "battery.re.ulv.potatos").addComponents(ElectricStats.createRechargeableBattery(Long.MAX_VALUE, GTValues.ULV)).setUnificationData(OrePrefix.battery, Tier.ULV).setCreativeTabs(GregTechAPI.TAB_GREGTECH_TOOLS);
+        POTATOS_ULV = exampleitems.addItem(2, "circuit.potatos").setUnificationData(OrePrefix.circuit, Tier.MAX);
     }
 }
--- src/main/java/ceuaddon/ExampleMaterials.java
+++ src/main/java/ceuaddon/ExampleMaterials.java
@@ -1,41 +1,39 @@
 package ceuaddon;
 
-import gregtech.api.GTValues;
-import gregtech.api.fluids.fluidType.FluidTypes;
 import gregtech.api.unification.material.Material;
-import gregtech.api.unification.material.properties.*;
+import gregtech.api.unification.material.properties.*; //recommended to check this if you want to add some properties
 import gregtech.api.unification.material.properties.BlastProperty.GasTier;
 import gregtech.api.unification.material.info.MaterialIconSet;
 
 import static gregtech.api.GTValues.*;
 import static gregtech.api.unification.material.Materials.*;
 import static gregtech.api.unification.material.info.MaterialFlags.*;
-import static gregtech.api.unification.material.info.MaterialIconSet.*;
 
 public class ExampleMaterials {
 
     public static final MaterialIconSet EXAMPLE = new MaterialIconSet("exampleiconset");
 
-//Registering them
+    //Registering them
     public static Material testMaterial;
 
-//Adding Materials
+    //Adding Materials
     public static void init() {
         testMaterial = new Material.Builder(32000, "test_material")
-            .ingot().fluid().dust()
-                //CEu uses the hex color code, but make sure to put 0x before the hex color
-            .iconSet(EXAMPLE)
-                //gregtech/api/unification/material/info/MaterialIconSet.java
-            .flags(EXT2_METAL, MORTAR_GRINDABLE, GENERATE_FINE_WIRE, GENERATE_ROTOR, GENERATE_FRAME, GENERATE_SMALL_GEAR, GENERATE_FOIL, GENERATE_GEAR)
-                //Flags, you can find all of them at gregtech/api/unification/material/info/MaterialFlags.java
-            .components(Titanium, 1, Oxygen, 20)
-                //You can find all materials on gregtech/api/unification/material/Materials.java
-            .itemPipeProperties(8192, 1) 
-                //Priority, transferRate
-            .cableProperties(VA[UV], 128, 0, true, 78) 
-                //Voltage, Amps, EnergyLoss, isSuperConductor, CriticalTemp
-            .blastTemp(9001, GasTier.HIGHER, VA[ZPM], 1000) 
-                //Temp, GasTier, eutOverride, durationOveride (in ticks)
-            .build();
+                .ingot()
+                .color(0xFF8D3B)
+                    //CEu uses the hex color code, but make sure to put 0x before the hex color
+                .iconSet(EXAMPLE)
+                    //gregtech/api/unification/material/info/MaterialIconSet.java
+                .flags(EXT2_METAL, MORTAR_GRINDABLE, GENERATE_FINE_WIRE, GENERATE_ROTOR, GENERATE_FRAME, GENERATE_SMALL_GEAR, GENERATE_FOIL, GENERATE_GEAR)
+                    //Flags, you can find all of them at gregtech/api/unification/material/info/MaterialFlags.java
+                .components(Titanium, 1, Oxygen, 20)
+                    //You can find all materials on gregtech/api/unification/material/Materials.java
+                .itemPipeProperties(8192, 1)
+                    //Priority, transferRate
+                .cableProperties(VA[UV], 128, 0, true, 78)
+                    //Voltage, Amps, EnergyLoss, isSuperConductor, CriticalTemp
+                .blastTemp(9001, GasTier.HIGHER, VA[ZPM], 1000)
+                    //Temp, GasTier, eutOverride, durationOveride (in ticks)
+                .build();
     }
 }
